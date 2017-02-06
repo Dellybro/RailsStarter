eeef8806b4def18525993a550ffe88dfdc3bd365
Travis Delly <Dellybro@Traviss-MacBook-Pro.local>
2017-02-05 23:39:44 -0700

    fix

--- config/initializers/S3Config.rb
+++ config/initializers/S3Config.rb
@@ -1,10 +1,10 @@
-$BUCKETNAME = "holdthecourt-photos"
+$BUCKETNAME = "photos"
 
 Aws.config.update({
   region: 'us-west-2',
-  credentials: Aws::Credentials.new(ENV["HTC_ACCESS_KEY_ID"], ENV["HTC_SECRET_ACCESS_KEY"])
+  credentials: Aws::Credentials.new(ENV["ACCESS_KEY_ID"], ENV["SECRET_ACCESS_KEY"])
 })
 
-s3 = Aws::S3::Client.new(region: 'us-west-2', credentials: Aws::Credentials.new(ENV["HTC_ACCESS_KEY_ID"], ENV["HTC_SECRET_ACCESS_KEY"]) )
+s3 = Aws::S3::Client.new(region: 'us-west-2', credentials: Aws::Credentials.new(ENV["ACCESS_KEY_ID"], ENV["SECRET_ACCESS_KEY"]) )
 AWSHelper = AwsHelper.new({s3: s3, bucket: $BUCKETNAME}) 
 
